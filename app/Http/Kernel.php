0328bd67b7964e3020ac6c99c642bd0fe9028622
Paul Vidal <paul.vidal.lujan@gmail.com>
2015-11-01 21:08:48 -0500

    changes 1

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -16,8 +16,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \App\Http\Middleware\VerifyCsrfToken::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class
     ];
 
     /**
@@ -26,6 +25,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'csrf' => \App\Http\Middleware\VerifyCsrfToken::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
