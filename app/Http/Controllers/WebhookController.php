01c948697cde70c6eea7d432508013d16e3a7b2e
Paul Vidal <paul.vidal.lujan@gmail.com>
2015-11-03 22:56:07 -0500

    changes

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -11,7 +11,7 @@ class WebhookController extends Controller
 {
     public function getIndex(Request $request)
     {
-        return "OK";
+        return "OK yess";
     }
 
     public function getWebhook()
